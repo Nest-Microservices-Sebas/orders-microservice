4f754c702db2d5fa5a93221077a9ded2b978b8db
Sebastian <sebastianvalbuena25@gmail.com>
2024-10-16 19:15:44 -0500

    Creando archivo docker para la bd

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('Orders-ms');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.port
+    }
+  });
+  
+  await app.listen();
+
+  logger.log(`Running on port ${envs.port}`);
+}
+bootstrap();
