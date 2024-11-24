9ae394fba7b36bc4b51d14866efea00c084555e3
Sebastian <sebastianvalbuena25@gmail.com>
2024-11-24 13:22:23 -0500

    Usando Nats

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,18 +3,19 @@ import * as joi from 'joi';
 
 interface EnvVars {
     PORT: number;
-    PRODUCTS_MICROSERVICE_HOST: string;
-    PRODUCTS_MICROSERVICE_PORT: number;
+    NATS_SERVER: string[];
 }
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
-    PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
-    PRODUCTS_MICROSERVICE_PORT: joi.number().required()
+    NATS_SERVER: joi.array().items(joi.string()).required()
 })
     .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+    ...process.env,
+    NATS_SERVER: process.env.NATS_SERVER?.split(',')
+});
 
 if (error)
     throw new Error(`Config validation error: ${error.message}`);
@@ -23,6 +24,5 @@ const envVars: EnvVars = value;
 
 export const envs = {
     port: envVars.PORT,
-    productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
-    productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
+    natsServers: envVars.NATS_SERVER
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { OrderPaginationDto } from './dto/order-pagination.dto';
 import { ChangeOrderStatusDto } from './dto/change-order-status.dto';
-import { PRODUCT_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
@@ -12,7 +12,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('OrdersService');
 
   constructor(
-    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy
   ) {
     super();
   }
@@ -27,7 +27,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
       // Confirm id products
       const productIds = createOrderDto.items.map(item => item.productId);
 
-      const products: any[] = await firstValueFrom(this.productsClient
+      const products: any[] = await firstValueFrom(this.client
         .send({ cmd: 'validate_products' }, productIds));
 
       // Calculate values
@@ -131,7 +131,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
 
     const productIds = order.OrderItem.map(orderItem => orderItem.productId);
 
-    const products: any[] = await firstValueFrom(this.productsClient
+    const products: any[] = await firstValueFrom(this.client
       .send({ cmd: 'validate_products' }, productIds));
 
     return {
